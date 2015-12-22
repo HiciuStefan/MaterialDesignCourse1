1f3f14d13645ec257cfeadfbb8e335e67afbfc8a
HiciuStefan <stefanhiciu@gmail.com>
2015-12-22 17:19:11 +0200

    * finised section 3

--- MyApplication/app/src/main/java/com/example/hiciu/materialtest1/fragments/NavigationDrawerFragment.java
+++ MyApplication/app/src/main/java/com/example/hiciu/materialtest1/fragments/NavigationDrawerFragment.java
@@ -11,9 +11,12 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.hiciu.materialtest1.Adapters.DrawerMainAdapter;
 import com.example.hiciu.materialtest1.Models.ModelDrawerMain;
@@ -61,23 +64,35 @@ public class NavigationDrawerFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
         recyclerView = (RecyclerView) view.findViewById(R.id.drawerList);
-        drawerAdapter = new DrawerMainAdapter(getContext(),getData());
+        drawerAdapter = new DrawerMainAdapter(getContext(), getData());
         recyclerView.setAdapter(drawerAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.addOnItemTouchListener(new RecycleItemClickListener(getContext(), recyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Toast.makeText(getContext(),"onClick "+ position,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+                Toast.makeText(getContext(),"onLongClick "+ position,Toast.LENGTH_SHORT).show();
+            }
+        }));
         return view;
     }
 
-    public static List<ModelDrawerMain> getData(){
+    public static List<ModelDrawerMain> getData() {
         List<ModelDrawerMain> data = new ArrayList<>();
-        int[] icons = {R.drawable.ball,R.drawable.mushroom,R.drawable.signoff,R.drawable.signoff,R.drawable.images};
-        String[] titles = {"title1","title2","title3","title4","title5"};
-        for (int i = 0; i < 100 ; i++) {
-            ModelDrawerMain model = new ModelDrawerMain(icons[i%icons.length],titles[i%titles.length]);
-            data.add(i, model );
+        int[] icons = {R.drawable.ball, R.drawable.mushroom, R.drawable.signoff, R.drawable.signoff, R.drawable.images};
+        String[] titles = {"title1", "title2", "title3", "title4", "title5"};
+        for (int i = 0; i < 100; i++) {
+            ModelDrawerMain model = new ModelDrawerMain(icons[i % icons.length], titles[i % titles.length]);
+            data.add(i, model);
         }
-        Log.d("GETTING DATA : ", "getData: "+data);
+        Log.d("GETTING DATA : ", "getData: " + data);
         return data;
     }
+
     public void setUp(int fragment_navigation_drawer, DrawerLayout drawerLayout, final Toolbar toolbar) {
         mContainerView = getActivity().findViewById(fragment_navigation_drawer);
         mDrawerLayout = drawerLayout;
@@ -125,5 +140,58 @@ public class NavigationDrawerFragment extends Fragment {
     public static String readFromPreferences(Context context, String preferenceName, String defaultValue) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, context.MODE_PRIVATE);
         return sharedPreferences.getString(preferenceName, defaultValue);
+    }
+
+    class RecycleItemClickListener implements RecyclerView.OnItemTouchListener {
+
+        private GestureDetector gestureDetector;
+        private ClickListener clickListener;
+
+        public RecycleItemClickListener(Context context, RecyclerView view, final ClickListener clickListener) {
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;/*super.onSingleTapUp(e);*/
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    /*super.onLongPress(e);*/
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null) {
+                        clickListener.onLongClick(child, recyclerView.getChildAdapterPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            View children = rv.findChildViewUnder(e.getX(), e.getY());
+            if (children != null && gestureDetector.onTouchEvent(e) && clickListener != null) {
+                clickListener.onClick(children, recyclerView.getChildAdapterPosition(children));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+
+
+    }
+
+    public static interface ClickListener {
+        public void onClick(View view, int position);
+
+        public void onLongClick(View view, int position);
     }
 }
