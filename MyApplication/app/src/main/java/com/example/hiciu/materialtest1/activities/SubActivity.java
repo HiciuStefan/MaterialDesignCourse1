0ae1e5ef243fd4eeefa3a14ea5fef40ded0f38f5
HiciuStefan <stefanhiciu@gmail.com>
2016-04-22 16:07:03 +0300

    *missing commits

--- MyApplication/app/src/main/java/com/example/hiciu/materialtest1/activities/SubActivity.java
+++ MyApplication/app/src/main/java/com/example/hiciu/materialtest1/activities/SubActivity.java
@@ -1,51 +1,326 @@
 package com.example.hiciu.materialtest1.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.NavUtils;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.example.hiciu.materialtest1.R;
+import com.example.hiciu.materialtest1.fragments.DummyFragment;
+import com.example.hiciu.materialtest1.tabs.SlidingTabLayout;
+import com.github.ksoichiro.android.observablescrollview.CacheFragmentStatePagerAdapter;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
+import com.github.ksoichiro.android.observablescrollview.Scrollable;
+import com.nineoldandroids.view.ViewHelper;
+import com.nineoldandroids.view.ViewPropertyAnimator;
 
-public class SubActivity extends AppCompatActivity {
+public class SubActivity extends AppCompatActivity implements ObservableScrollViewCallbacks {
+
+    private View mHeaderView;
+    private View myImage;
+    private View mToolbarView;
+    private int mBaseTranslationY;
+    private ViewPager mPager;
+    private NavigationAdapter mPagerAdapter;
+    private View mRootView;
+    /*private View mMutama;*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        myImage = findViewById(R.id.myImage);
+        mHeaderView = findViewById(R.id.header);
+        mRootView = findViewById(R.id.root);
+        ViewCompat.setElevation(mHeaderView, getResources().getDimension(R.dimen.toolbar_elevation));
+        /*mHeaderView.post(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                mHeaderView.setPadding(0,mToolbarView.getHeight(),0,0);
+            }
+        });*/
+        /*mMutama = findViewById(R.id.mutama);*/
+        mToolbarView = findViewById(R.id.toolbar);
+        mPagerAdapter = new NavigationAdapter(getSupportFragmentManager());
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mPagerAdapter);
+
+        SlidingTabLayout tabs = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        tabs.setCustomTabView(R.layout.custom_tab_view, R.id.tabText);
+        /*tabs.setSelectedIndicatorColors(getResources().getColor(R.color.accent));*/
+        tabs.setDistributeEvenly(true);
+        tabs.setViewPager(mPager);
+/*        slidingTabLayout.*/
+
+        // When the page is selected, other fragments' scrollY should be adjusted
+        // according to the toolbar status(shown/hidden)
+        tabs.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i2) {
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                propagateToolbarState(toolbarIsShown());
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
             }
         });
+
+        propagateToolbarState(toolbarIsShown());
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sub_activity, menu);
-        return true;
-    }
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+        if (dragging) {
+            int toolbarHeight = getScrollableHeight();
+            float currentHeaderTranslationY = ViewHelper.getTranslationY(mHeaderView);
+            if (firstScroll) {
+                if (-toolbarHeight < currentHeaderTranslationY) {
+                    mBaseTranslationY = scrollY;
+                }
+            }
+            float headerTranslationY = ScrollUtils.getFloat(-(scrollY - mBaseTranslationY), -toolbarHeight, 0);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            NavUtils.navigateUpFromSameTask(this);
+            ViewPropertyAnimator.animate(myImage).cancel();
+            ViewHelper.setTranslationY(myImage, headerTranslationY);
+            ViewPropertyAnimator.animate(mHeaderView).cancel();
+            ViewHelper.setTranslationY(mHeaderView, headerTranslationY);
+
+            /*ViewPropertyAnimator.animate(mMutama).cancel();
+            ViewHelper.setTranslationY(mMutama, headerTranslationY);*/
+
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    private int getScrollableHeight() {
+        return myImage.getHeight() /*+ mToolbarView.getHeight()*/;
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+        mBaseTranslationY = 0;
+
+        Fragment fragment = getCurrentFragment();
+        if (fragment == null) {
+            return;
+        }
+        View view = fragment.getView();
+        if (view == null) {
+            return;
+        }
+
+        // ObservableXxxViews have same API
+        // but currently they don't have any common interfaces.
+        adjustToolbar(scrollState, view);
+    }
+
+    private void adjustToolbar(ScrollState scrollState, View view) {
+        int toolbarHeight = getScrollableHeight();
+        final Scrollable scrollView = (Scrollable) view.findViewById(R.id.scroll);
+        if (scrollView == null) {
+            return;
+        }
+        int scrollY = scrollView.getCurrentScrollY();
+        if (scrollState == ScrollState.DOWN) {
+            showToolbar();
+        } else if (scrollState == ScrollState.UP) {
+            if (toolbarHeight <= scrollY) {
+                hideToolbar();
+            } else {
+                showToolbar();
+            }
+        } else {
+            // Even if onScrollChanged occurs without scrollY changing, toolbar should be adjusted
+            if (toolbarIsShown() || toolbarIsHidden()) {
+                // Toolbar is completely moved, so just keep its state
+                // and propagate it to other pages
+                propagateToolbarState(toolbarIsShown());
+            } else {
+                // Toolbar is moving but doesn't know which to move:
+                // you can change this to hideToolbar()
+                showToolbar();
+            }
+        }
+    }
+
+    private Fragment getCurrentFragment() {
+        return mPagerAdapter.getItemAt(mPager.getCurrentItem());
+    }
+
+    private void propagateToolbarState(boolean isShown) {
+        int toolbarHeight = getScrollableHeight();
+
+        // Set scrollY for the fragments that are not created yet
+        mPagerAdapter.setScrollY(isShown ? 0 : toolbarHeight);
+
+        // Set scrollY for the active fragments
+        for (int i = 0; i < mPagerAdapter.getCount(); i++) {
+            // Skip current item
+            if (i == mPager.getCurrentItem()) {
+                continue;
+            }
+
+            // Skip destroyed or not created item
+            Fragment f = mPagerAdapter.getItemAt(i);
+            if (f == null) {
+                continue;
+            }
+
+            View view = f.getView();
+            if (view == null) {
+                continue;
+            }
+            propagateToolbarState(isShown, view, toolbarHeight);
+        }
+    }
+
+    private void propagateToolbarState(boolean isShown, View view, int toolbarHeight) {
+        Scrollable scrollView = (Scrollable) view.findViewById(R.id.scroll);
+        if (scrollView == null) {
+            return;
+        }
+        if (isShown) {
+            // Scroll up
+            if (0 < scrollView.getCurrentScrollY()) {
+                scrollView.scrollVerticallyTo(0);
+            }
+        } else {
+            // Scroll down (to hide padding)
+            if (scrollView.getCurrentScrollY() < toolbarHeight) {
+                scrollView.scrollVerticallyTo(toolbarHeight);
+            }
+        }
+    }
+
+    private boolean toolbarIsShown() {
+        return ViewHelper.getTranslationY(mHeaderView) == 0;
+    }
+
+    private boolean toolbarIsHidden() {
+        return ViewHelper.getTranslationY(mHeaderView) == -getScrollableHeight();
+    }
+
+    private void showToolbar() {
+        float headerTranslationY = ViewHelper.getTranslationY(mHeaderView);
+        if (headerTranslationY != 0) {
+            ViewPropertyAnimator.animate(myImage).cancel();
+            ViewPropertyAnimator.animate(myImage).translationY(0).setDuration(200).start();
+            ViewPropertyAnimator.animate(mHeaderView).cancel();
+            ViewPropertyAnimator.animate(mHeaderView).translationY(0).setDuration(200).start();
+
+/*            ViewPropertyAnimator.animate(mMutama).cancel();
+            ViewPropertyAnimator.animate(mMutama).translationY(0).setDuration(200).start();*/
+        }
+        propagateToolbarState(true);
+    }
+
+    private void hideToolbar() {
+        float headerTranslationY = ViewHelper.getTranslationY(mHeaderView);
+        int toolbarHeight = getScrollableHeight();
+        if (headerTranslationY != -toolbarHeight) {
+            ViewPropertyAnimator.animate(myImage).cancel();
+            ViewPropertyAnimator.animate(myImage).translationY(-toolbarHeight).setDuration(200).start();
+            ViewPropertyAnimator.animate(mHeaderView).cancel();
+            ViewPropertyAnimator.animate(mHeaderView).translationY(-toolbarHeight).setDuration(200).start();
+/*            ViewPropertyAnimator.animate(mMutama).cancel();
+            ViewPropertyAnimator.animate(mMutama).translationY(-toolbarHeight).setDuration(200).start();*/
+
+        }
+        propagateToolbarState(false);
+    }
+
+    /**
+     * This adapter provides two types of fragments as an example.
+     * {@linkplain #createItem(int)} should be modified if you use this example for your app.
+     */
+    private static class NavigationAdapter extends CacheFragmentStatePagerAdapter {
+
+        private static final String[] TITLES = new String[]{"Applepie", "Butter Cookie", "Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb", "Ice Cream Sandwich", "Jelly Bean", "KitKat", "Lollipop"};
+
+        private int mScrollY;
+
+        public NavigationAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void setScrollY(int scrollY) {
+            mScrollY = scrollY;
+        }
+
+        @Override
+        protected Fragment createItem(int position) {
+            // Initialize fragments.
+            // Please be sure to pass scroll position to each fragments using setArguments.
+            Fragment f;
+            final int pattern = position % 4;
+            switch (pattern) {
+                default: {
+                    f = new DummyFragment();
+                    if (0 <= mScrollY) {
+                        Bundle args = new Bundle();
+                        /*args.putInt(ViewPagerTabScrollViewFragment.ARG_SCROLL_Y, mScrollY);*/
+                        f.setArguments(args);
+                    }
+                    break;
+                }
+               /* case 1: {
+                    f = new ViewPagerTabListViewFragment();
+                    if (0 < mScrollY) {
+                        Bundle args = new Bundle();
+                        args.putInt(ViewPagerTabListViewFragment.ARG_INITIAL_POSITION, 1);
+                        f.setArguments(args);
+                    }
+                    break;
+                }
+                case 2: {
+                    f = new ViewPagerTabRecyclerViewFragment();
+                    if (0 < mScrollY) {
+                        Bundle args = new Bundle();
+                        args.putInt(ViewPagerTabRecyclerViewFragment.ARG_INITIAL_POSITION, 1);
+                        f.setArguments(args);
+                    }
+                    break;
+                }
+                case 3:
+                default: {
+                    f = new ViewPagerTabGridViewFragment();
+                    if (0 < mScrollY) {
+                        Bundle args = new Bundle();
+                        args.putInt(ViewPagerTabGridViewFragment.ARG_INITIAL_POSITION, 1);
+                        f.setArguments(args);
+                    }
+                    break;
+                }*/
+            }
+            return f;
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
     }
 }
+
+
